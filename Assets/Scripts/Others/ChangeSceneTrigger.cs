d0d69ef6d2e8e7e756724e438dbf2d96b2de42e6
Dalmiro <dalmirolunadapozo@gmail.com>
2021-11-20 16:05:23 -0300

    Arreglado bugs de escenas y del sistema de combos

--- Assets/Scripts/Others/ChangeSceneTrigger.cs
+++ Assets/Scripts/Others/ChangeSceneTrigger.cs
@@ -2,19 +2,12 @@ using UnityEngine;
 
 public class ChangeSceneTrigger : MonoBehaviour
 {
-    public bool repeatScene;
-    public bool toGameplay;
-
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("aAGadg");
-        
         if (collision.gameObject.tag == "Player")
         {
-
             SceneController.LoadScene("Gameplay", 0.5f, 1f);
-
         }
     }
     
@@ -22,11 +15,7 @@ public class ChangeSceneTrigger : MonoBehaviour
     {
         SceneController.LoadScene(name, 0.5f, 1f);
     }
-    public void ChangeSceneToGameplay()
-    {
-        Time.timeScale = 1;
-        SceneController.LoadScene("Gameplay", 0.5f, 1f);
-    }
+   
 
 
 }
